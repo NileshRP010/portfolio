b26f5ad9bcecc6e2bf1af691a13af80eadf452cd
Nilesh Pathak <nileshrajpathak00@gmail.com>
2025-03-22 04:29:47 +0530

    Initial commit

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Home = () => {
+  const [displayText, setDisplayText] = useState("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃ");
+  const finalText = "Hey there! I'm Nilesh 👋🏼";
+  const japaneseChars = "ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ";
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const intervalId = setInterval(() => {
+      if (currentIndex <= finalText.length) {
+        setDisplayText((prevText) => {
+          const revealedText = finalText.slice(0, currentIndex);
+          const remainingLength = finalText.length - revealedText.length;
+          const randomChars = Array.from(
+            { length: remainingLength },
+            () =>
+              japaneseChars[Math.floor(Math.random() * japaneseChars.length)]
+          ).join("");
+          return revealedText + randomChars;
+        });
+        currentIndex++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 100);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative"
+    >
+      <RevealOnScroll>
+        <div className="glitch-wrapper mb-4">
+          <h1
+            className="text-3xl sm:text-4xl font-bold glitch layers text-center sm:text-left"
+            data-text={displayText}
+          >
+            <span>
+              {displayText.split("").map((char, index) => (
+                <span key={index}>{char}</span>
+              ))}
+            </span>
+          </h1>
+        </div>
+
+        <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+        I'm a full-stack developer passionate about building clean, scalable web applications.
+         I focus on delivering high-performance solutions with a seamless user experience
+         while leveraging data structures and algorithms to solve complex problems efficiently.
+        </p>
+
+        <div className="flex justify-center space-x-4">
+          <a
+            href="#projects"
+            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+          >
+            View Projects
+          </a>
+
+          <a
+            href="#contact"
+            className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
+          >
+            Contact Me
+          </a>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,167 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+             Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> GedgetHub</h3>
+              <p className="text-gray-400 mb-4">
+              GadgetHub is a sleek web app for discovering, comparing, and reviewing the latest tech gadgets. 
+              It offers a seamless user experience with real-time updates and smart recommendations
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Node.js", "MongoDB", "Cloudinary"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2"> 3D Image-Gallery</h3>
+              <p className="text-gray-400 mb-4">
+                Interactive 3D image gallery with advanced filtering and search features.
+                and modern user interface.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["JavaScript", "Tailwind", "HTML", "CSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Netflix Clone</h3>
+              <p className="text-gray-400 mb-4">
+                Full-stack Netflix clone with modern UI, 
+                user authentication, and real-time movie streaming.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Node.js", "JavaScript", "MongoDB", "Cloudinary"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+              <p className="text-gray-400 mb-4">
+                Scalable chat platform supporting real-time messaging, presence,
+                and group chat features.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Socket.IO", "Express", "React", "Zustand","DaisyUI","Tailwind"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
