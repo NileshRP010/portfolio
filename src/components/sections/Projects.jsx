ce4fa5743e4a1933dbdf80d60b48ed1b99325281
Nilesh Raj Pathak <nileshpathak781@gmail.com>
2025-08-18 01:03:00 +0530

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,13 +14,13 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> GedgetHub</h3>
+              <h3 className="text-xl font-bold mb-2">Wander AI</h3>
               <p className="text-gray-400 mb-4">
-              GadgetHub is a sleek web app for discovering, comparing, and reviewing the latest tech gadgets. 
-              It offers a seamless user experience with real-time updates and smart recommendations
+              AI-Powered Travel Planner – Generate stunning, personalized day-by-day itineraries with Google Gemini AI. 
+                Built with React.js, Tailwind CSS, Firebase, and smooth animations for an inspiring trip-planning experience.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "MongoDB", "Cloudinary"].map((tech, key) => (
+                {["React", "Tailwind CSS", "Firebase","Gemini AI"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -34,10 +34,10 @@ export const Projects = () => {
 
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="wander-ai-plum.vercel.app"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
-                  View Project →
+                  View Project → 
                 </a>
               </div>
             </div>
@@ -51,8 +51,7 @@ export const Projects = () => {
             >
               <h3 className="text-xl font-bold mb-2"> 3D Image-Gallery</h3>
               <p className="text-gray-400 mb-4">
-                Interactive 3D image gallery with advanced filtering and search features.
-                and modern user interface.
+               
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
                 {["JavaScript", "Tailwind", "HTML", "CSS"].map((tech, key) => (
